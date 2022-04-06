daa19fbf640206841829eac47d5e745a3d7c3250
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-06 15:17:04 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 var request = require('supertest');
-var app = require('../app.js');
-describe('GET /', function() {
- it('respond with hello world', function(done) {
-  //navigate to root and check the response is "hello world"
-  request(app).get('/').expect('hello world', done);
- });
-});
+var app = require('../index');
+
+describe('node-app', () => {
+    after(() => {
+        require('./../index').stop();
+    })
+
+    describe('GET /', function() {
+        it('respond with hello konvergen', (done) => {
+            //navigate to root and check the response is "hello world"
+            request(app).get('/').expect('hello konvergen').end(done);
+        });
+    });
+})

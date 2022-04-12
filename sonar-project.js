88d42fd9b8456495ec825fd1c0ace2414911cbe8
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-12 21:12:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,8 +3,8 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
   serverUrl: 'http://15.207.99.1:9000/',
        options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectDescription': 'node-app',
+	    'sonar.projectName': 'node-app',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': '84283ade512912faf58ed9e1afe6e58b9a26fee5',
 	    //'sonar.login': 'admin',

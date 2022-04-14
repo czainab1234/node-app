066c618479476f8ba07f70cb5d7ff098fba7993c
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-14 17:12:43 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,1 @@
-sonar.projectKey=Pipeline node-js -pipeline
-sonar.projectName=Pipeline node-js -pipeline
-sonar.login=de0301291b2a21ed54b03f849d469628b65a4472
-//sonar.password=dN&]sQIjrL7pZ2rJ
-#sonar.exclusions=vendor/**, storage/**, resources/**,, **/*.java
-sonar.sources=/var/lib/jenkins/workspace/Pipeline node-js -pipeline/
-#sonar.language=js
-//sonar.host.url=https://stg-codescanner.ottplay.com
+

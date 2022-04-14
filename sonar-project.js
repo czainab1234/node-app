4b89081db2c68fb75b641a796a8d39e854dd5994
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-14 17:33:43 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,1 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
 
+sonarqubeScanner({
+  serverUrl: 'http://3.110.177.134:9000/',
+       options : {
+	    'sonar.projectDescription': 'Pipeline node-js -pipeline',
+	    'sonar.projectName': 'Pipeline node-js -pipeline',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': efc2443e6cf06a342f144bf5abad17ef6f545328'',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
